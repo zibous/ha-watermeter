1db4aae039c503bde3db1aadbc66cc35233a343d
Peter Siebler <peter.siebler@gmail.com>
2021-12-17 10:41:15 +0100

    Version 1.0.2
    new check payload, Exception logging with additional info.

--- lib/calculator.py
+++ lib/calculator.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
-import time
-import paho.mqtt.publish as publish
-from datetime import datetime
 
-from lib import logger
-from config import *
+import sys
+
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 is required to run")
+
+try:
+    import json
+    import time
+    import paho.mqtt.publish as publish
+    from datetime import datetime
+
+    from lib import logger
+    from config import *
+
+except Exception as e:
+    print('Import error {}, check requirements.txt'.format(e))
+
 
 log = logger.Log(__name__, LOG_LEVEL, LOG_DIR)
 
@@ -42,6 +53,13 @@ class Calculator:
             return None
         return now.strftime(DATEFORMAT_CURRENT)
 
+    def __checkPayload__(self, key, default):
+        if key in self.payload:
+            return self.payload[key]
+        else:
+            log.debug(f'Missing key {key} - used default value {default}')
+            return default
+
     def __doCalc__(self):
         """Calculate all data based on the current payload
         """
@@ -54,103 +72,108 @@ class Calculator:
             log.info('✔︎ Start calculation')
 
             # "timestamp": "2020-07-15T07:34:17Z"
-            current_dts = datetime.strptime(self.payload['timestamp'], DATEFORMAT_TIMESTAMP)
-            previous_dts = datetime.strptime(self.pd['timestamp'], DATEFORMAT_TIMESTAMP)
-            lastmonth_dts = datetime.strptime(self.payload['last_month_measure_date'], DATEFORMAT_DAY)
+
+            current_dts = datetime.strptime(self.__checkPayload__('timestamp', now.strftime(DATEFORMAT_TIMESTAMP)), DATEFORMAT_TIMESTAMP)
+            previous_dts = datetime.strptime(self.__checkPayload__('timestamp', now.strftime(DATEFORMAT_TIMESTAMP)), DATEFORMAT_TIMESTAMP)
+
+            lastmonth_dts = datetime.strptime(self.__checkPayload__('last_month_measure_date', now.strftime(DATEFORMAT_TIMESTAMP)), DATEFORMAT_DAY)
             self.elapsed_time = current_dts - previous_dts
 
             # watermeter display data
             self.cd['date'] = current_dts.strftime(DATEFORMAT_DAY)
             self.cd['time'] = current_dts.strftime(TIME_FORMAT)
             # current water meter counter value
-            self.cd['total_m3'] = float(self.payload['total_m3'])
-            meterM3 = float(round(self.payload['total_m3'], 3))
+            self.cd['total_m3'] = float(self.__checkPayload__('total_m3', 0.00))
+            meterM3 = float(round(self.cd['total_m3'], 3))
 
             # the last periode data
             self.cd['last_total']['month'] = lastmonth_dts.strftime(DATEFORMAT_MONTH)
-            self.cd['last_total']['month_m3'] = float(self.payload['last_month_total_m3'])
+            self.cd['last_total']['month_m3'] = float(self.__checkPayload__('last_month_total_m3', 0.00))
 
             # current / prev m3
             if(self.cd['last_total']['month_m3'] > 0.00):
-                self.cd['ratio'] = round(float(meterM3 / self.cd['last_total']['month_m3']), 2)
+                self.cd['ratio']=round(float(meterM3 / self.cd['last_total']['month_m3']), 2)
 
             # calculate the m3
             if(self.pd):
 
                 log.info('✔︎ new data calculation')
 
-                self.cd['m3']['current'] = round(meterM3 - float(self.pd['total_m3']), 3)
-                self.cd['m3']['hour'] = round(meterM3 - float(self.pd['last_total']['hour_m3']), 3)
-                self.cd['m3']['day'] = round(meterM3 - float(self.pd['last_total']['day_m3']), 3)
-                self.cd['m3']['month'] = round(meterM3 - float(self.pd['last_total']['month_m3']), 3)
-                self.cd['m3']['year'] = round(meterM3 - float(self.pd['last_total']['year_m3']), 3)
+                self.cd['m3']['current']=round(meterM3 - float(self.pd['total_m3']), 3)
+                self.cd['m3']['hour']=round(meterM3 - float(self.pd['last_total']['hour_m3']), 3)
+                self.cd['m3']['day']=round(meterM3 - float(self.pd['last_total']['day_m3']), 3)
+                self.cd['m3']['month']=round(meterM3 - float(self.pd['last_total']['month_m3']), 3)
+                self.cd['m3']['year']=round(meterM3 - float(self.pd['last_total']['year_m3']), 3)
 
                 # caclulate the liter
-                self.cd['liter']['current'] = round(float(self.cd['m3']['current']) * 1000.00, 2)
-                self.cd['liter']['hour'] = round(float(self.cd['m3']['hour']) * 1000.00, 2)
-                self.cd['liter']['day'] = round(float(self.cd['m3']['day']) * 1000.00, 2)
-                self.cd['liter']['month'] = round(float(self.cd['m3']['month']) * 1000.00, 2)
-                self.cd['liter']['year'] = round(float(self.cd['m3']['year']) * 1000.00, 2)
+                self.cd['liter']['current']=round(float(self.cd['m3']['current']) * 1000.00, 2)
+                self.cd['liter']['hour']=round(float(self.cd['m3']['hour']) * 1000.00, 2)
+                self.cd['liter']['day']=round(float(self.cd['m3']['day']) * 1000.00, 2)
+                self.cd['liter']['month']=round(float(self.cd['m3']['month']) * 1000.00, 2)
+                self.cd['liter']['year']=round(float(self.cd['m3']['year']) * 1000.00, 2)
 
                 # check update last total for hour
-                if(current_dts.strftime(DATEFORMAT_HOUR) != previous_dts.strftime(DATEFORMAT_HOUR) ):
+                if(current_dts.strftime(DATEFORMAT_HOUR) != previous_dts.strftime(DATEFORMAT_HOUR)):
                     # new hour
                     log.debug(f"✔︎ Update last total hour: {current_dts.strftime(DATEFORMAT_HOUR)} prev: {previous_dts.strftime(DATEFORMAT_HOUR)}")
-                    self.cd['last_total']['hour'] = current_dts.strftime(DATEFORMAT_HOUR)
-                    self.cd['last_total']['hour_m3'] = meterM3
+                    self.cd['last_total']['hour']=current_dts.strftime(DATEFORMAT_HOUR)
+                    self.cd['last_total']['hour_m3']=meterM3
                     # new hour, store the previous data to
                     self.__printLogData()
                 else:
-                    self.cd['last_total']['hour'] = self.pd['last_total']['hour']
-                    self.cd['last_total']['hour_m3'] = self.pd['last_total']['hour_m3']
+                    self.cd['last_total']['hour']=self.pd['last_total']['hour']
+                    self.cd['last_total']['hour_m3']=self.pd['last_total']['hour_m3']
 
                 # check update last total for day
                 if(current_dts.strftime(DATEFORMAT_DAY) != previous_dts.strftime(DATEFORMAT_DAY)):
                     # new day
                     log.debug(f"✔︎ Update last total day: {current_dts.strftime(DATEFORMAT_DAY)} prev: {previous_dts.strftime(DATEFORMAT_DAY)}")
-                    self.cd['last_total']['day'] = current_dts.strftime(DATEFORMAT_DAY)
-                    self.cd['last_total']['day_m3'] = meterM3
+                    self.cd['last_total']['day']=current_dts.strftime(DATEFORMAT_DAY)
+                    self.cd['last_total']['day_m3']=meterM3
                     self.__savedayreport__()
                 else:
-                    self.cd['last_total']['day'] = self.pd['last_total']['day']
-                    self.cd['last_total']['day_m3'] = self.pd['last_total']['day_m3']
+                    self.cd['last_total']['day']=self.pd['last_total']['day']
+                    self.cd['last_total']['day_m3']=self.pd['last_total']['day_m3']
 
                 # check update last total for month
                 if(current_dts.strftime(DATEFORMAT_MONTH) != previous_dts.strftime(DATEFORMAT_MONTH)):
                     # new month
                     log.debug(f"✔︎ Update last total day: {current_dts.strftime(DATEFORMAT_MONTH)} prev: {previous_dts.strftime(DATEFORMAT_MONTH)}")
-                    self.cd['last_total']['month'] = current_dts.strftime(DATEFORMAT_MONTH)
-                    self.cd['last_total']['month_m3'] = meterM3
+                    self.cd['last_total']['month']=current_dts.strftime(DATEFORMAT_MONTH)
+                    self.cd['last_total']['month_m3']=meterM3
                     # new month, store the reportdata
                     self.__savereportdata__()
                 else:
-                    self.cd['last_total']['month'] = self.pd['last_total']['month']
-                    self.cd['last_total']['month_m3'] = self.pd['last_total']['month_m3']
+                    self.cd['last_total']['month']=self.pd['last_total']['month']
+                    self.cd['last_total']['month_m3']=self.pd['last_total']['month_m3']
 
                 # check update last total for year
                 if(current_dts.strftime(DATEFORMAT_YEAR) != previous_dts.strftime(DATEFORMAT_YEAR)):
                     # new year
                     log.debug(f"✔︎ Update last total day: {current_dts.strftime(DATEFORMAT_YEAR)} prev: {previous_dts.strftime(DATEFORMAT_YEAR)}")
-                    self.cd['last_total']['year'] = current_dts.strftime(DATEFORMAT_YEAR)
-                    self.cd['last_total']['year_m3'] = meterM3
+                    self.cd['last_total']['year']=current_dts.strftime(DATEFORMAT_YEAR)
+                    self.cd['last_total']['year_m3']=meterM3
                 else:
-                    self.cd['last_total']['year'] = self.pd['last_total']['year']
-                    self.cd['last_total']['year_m3'] = self.pd['last_total']['year_m3']
+                    self.cd['last_total']['year']=self.pd['last_total']['year']
+                    self.cd['last_total']['year_m3']=self.pd['last_total']['year_m3']
+
+            self.cd['wmbusmeter']=self.payload
 
             # add periodes and timestamps
             log.debug('update periodes and timestamps')
-            self.cd['timestamp'] = self.payload['timestamp']
-            self.cd['periode'] = now.strftime(DATEFORMAT_DAY)
-            self.cd['month'] = now.strftime(DATEFORMAT_MONTH)
-            self.cd['year'] = now.strftime(DATEFORMAT_YEAR)
-            self.cd['elapsed_time'] = str(self.elapsed_time)
-            self.cd['last_update'] = now.strftime(DATEFORMAT_CURRENT)
-            self.cd['data_provider'] = APP_STATEINFO['hostname']
+            self.cd['timestamp']=self.__checkPayload__('timestamp', now.strftime(DATEFORMAT_TIMESTAMP))
+            self.cd['periode']=now.strftime(DATEFORMAT_DAY)
+            self.cd['month']=now.strftime(DATEFORMAT_MONTH)
+            self.cd['year']=now.strftime(DATEFORMAT_YEAR)
+            self.cd['elapsed_time']=str(self.elapsed_time)
+            self.cd['last_update']=now.strftime(DATEFORMAT_CURRENT)
+            self.cd['data_provider']=APP_STATEINFO['hostname']
 
             # water leak status
             log.debug('update water leak status')
-            self.cd['alarm'] = self.payload['current_alarms']
-            self.cd['last_alarm'] = self.payload['previous_alarms']
+
+            self.cd['alarm']=self.__checkPayload__('current_alarms', 'no alarm')
+            self.cd['last_alarm']=self.__checkPayload__('previous_alarms', 'no alarm')
 
             # publish the new data
             log.info(f'✔︎ Publish the new data {MQTT_PUBLISH_TOPIC}')
@@ -161,8 +184,8 @@ class Calculator:
 
             return True
 
-        except BaseException:
-            log.error(f"Error on calculation")
+        except BaseException as e:
+            log.error(f"Error on calculation: {__name__},  line: {e.__traceback__.tb_lineno}, message: {str(e)}")
             return False
 
     def __loaddata__(self):
@@ -171,13 +194,13 @@ class Calculator:
             if (self.cdfilename):
                 if os.path.isfile(self.cdfilename):
                     with open(self.cdfilename, "r") as f:
-                        _prevdat = f.read()
+                        _prevdat=f.read()
                         log.debug(f"✔︎ previous data loaded {self.cdfilename}")
-                        self.pd = json.loads(_prevdat)
+                        self.pd=json.loads(_prevdat)
                     return True
                 else:
                     log.debug(f"✔︎ init previous data, datafile not found!")
-                    self.pd = self.cd
+                    self.pd=self.cd
                     return True
         except BaseException:
             log.error(f"Error reading file {self.cdfilename}")
@@ -203,7 +226,7 @@ class Calculator:
     def __savedayreport__(self):
         if(self.dayreportfile):
             try:
-                datalist = (
+                datalist=(
                     self.pd['device'],
                     self.pd['deviceid'],
                     self.pd['month'],
